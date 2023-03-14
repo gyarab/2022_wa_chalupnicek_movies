abe541e1f5ac7fa034c4acfad491e4a528e6522d
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2023-03-14 09:32:09 +0100

    added fixtures

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Movie
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'year']
+    list_display_links = ['name']
+    search_fields = ['name']
+
+admin.site.register(Movie, MovieAdmin)
--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -3,4 +3,9 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
-    description = models.TextField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.year})"
+
+    
