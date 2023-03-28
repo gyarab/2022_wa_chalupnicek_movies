488197759a8809c2aca5c15e6470692374ec4cfd
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2023-03-28 09:25:42 +0200

    added homepage view and template

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Movie
 
+def homepage(request):
+    context = {
+        "movies": Movie.objects.all()
+    }
+
+    return render(request, 'main.html', context)
 # Create your views here.
--- gamdb/gamdb/urls.py
+++ gamdb/gamdb/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from films.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage, name="homepage")
 ]
