cbac572ddf9e35ee27070f3c7e08d980dd0ddac9
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2023-04-25 09:27:59 +0200

    added movie filter and search

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Movie, Director, Actor, Genre
+from django.db.models import Q
 
 def directors(request):
     context = {
@@ -15,8 +16,19 @@ def director(request, id):
     return render(request, 'director.html', context)
 
 def movies(request):
+    movies_queryset = Movie.objects.all()
+    genre = request.GET.get('genre')
+    if genre:
+        movies_queryset = movies_queryset.filter(genres__name=genre)
+    search = request.GET.get('search')
+    if search:
+        movies_queryset = movies_queryset.filter(Q(name__icontains=search)|Q(description__icontains=search)) 
+
     context = {
-        "movies": Movie.objects.all()
+        "movies": movies_queryset,
+        "genres": Genre.objects.all().order_by('name'),
+        "genre": genre,
+        "search": search,
     }
     return render(request, 'movies.html', context)
 
