89c4d16e2a02e1363d0f7f50e35814c1b50eb55b
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2023-05-09 08:58:07 +0200

    prettify comment form

--- gamdb/films/forms.py
+++ gamdb/films/forms.py
@@ -1,6 +1,20 @@
 from django import forms
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class CommentForm(forms.Form):
-    author = forms.CharField(required=False)
-    text = forms.CharField(required=False, widget=forms.Textarea(attrs={"rows": 5}))
-    rating = forms.IntegerField(required=False)
+    author = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Pepa z Depa"})
+    )
+    text = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={"rows": 5, "class": "form-control"})
+    )
+    rating = forms.IntegerField(
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ]
+    )
